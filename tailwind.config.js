e92aac93c4d69d4dafbf0c163d5aac93d02a48a1
killcrafter1 <kilian-hauber@gmx.de>
2021-11-04 21:48:20 +0100

    tailwindcss-website-example

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        // define your own colors here, example:
+        "mycolor": "#A259FF",
+      },
+      fontFamily: {
+
+      }
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+
+  }
+}
